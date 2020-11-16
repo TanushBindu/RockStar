53bf84f1d39b3dd2f0c282d3e2d6e93d0bcc82dc
rbindu <rbindu@192.168.0.4>
2020-11-17 00:23:24 +0530

    Committing RockStar Sample project

---
+++ src/sample/Demo.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class Demo 
+{
+	public static void main(String args[])
+	{
+		//word();
+		System.out.println("Hello World");
+		
+	}
+
+	private static void word() 
+	{
+	
+		System.out.println("Hi Kanthu");
+		System.out.println(" :Add two words = "+"2+2+5");
+	
+	}
+}
